8ad307ca265179e57b694da2a0b8415ef7b8c341
Ryan Dao <daoduyducduong@gmail.com>
2012-08-11 19:13:10 -0700

    Add error handling for PayPalDirect

--- code/PayPalDirect.php
+++ code/PayPalDirect.php
@@ -12,53 +12,63 @@ class PayPalDirectGateway extends PayPalGateway {
     // Add credit card data. May have to parse the data to fit PayPal's format
     $ccTypeMap = $this->creditCardTypeIDMapping();
     $this->postData['CREDITCARDTYPE'] = $ccTypeMap[$data['CreditCardType']];
-    $this->postData['ACCT'] = $data['CardNumner'];
+    $this->postData['ACCT'] = $data['CardNumber'];
     $this->postData['EXPDATE'] = $data['MonthExpiry'] . $data['YearExpiry'];
     //$this->postData['CVV2'] = $data['Cvv2'];
     $this->postData['FIRSTNAME'] = $data['FirstName'];
     $this->postData['LASTNAME'] = $data['LastName'];
 
-    // Add optional parameters 
+    // Add optional parameters
     $this->postData['IP'] = isset($data['IP']) ? $data['IP'] : $_SERVER['REMOTE_ADDR'];
 
     // Post the data to PayPal server
     $response = $this->postPaymentData($this->postData);
-    $responseArr = $this->parseResponse($response);
-    
-    switch ($responseArr['ACK']) {
-      case self::SUCCESS_CODE:
-      case self::SUCCESS_WARNING:
-        return new PaymentGateway_Result();
-        break;
-      case self::FAILURE_CODE:
-        return new PaymentGateway_Result(false);
-        break;
-      default:
-        return new PaymentGateway_Result(false);
-        break;
-    }   
+    if ($response->getStatusCode() != '200') {
+      // Cannot connect to PayPal server
+      return new PaymentGateway_Failure($response);
+    } else {
+      $responseArr = $this->parseResponse($response);
+
+      if (! isset($responseArr['ACK'])) {
+        return new PaymentGateway_Failure();
+      } else {
+        switch ($responseArr['ACK']) {
+          case self::SUCCESS_CODE:
+          case self::SUCCESS_WARNING:
+            return new PaymentGateway_Success();
+            break;
+          case self::FAILURE_CODE:
+            $errorList = $this->getErrors($response);
+            return new PaymentGateway_Failure(null, null, $errorList);
+            break;
+          default:
+            return new PaymentGateway_Failure();
+            break;
+        }
+      }
+    }
   }
 }
 
 class PayPalDirectGateway_Mock extends PayPalDirectGateway {
-  
+
   /**
    * The response template string for PayPalDirect
-   * 
+   *
    * @var String
    */
   private $responseTemplate = 'TIMESTAMP=&CORRELATIONID=&ACK=&VERSION=&BUILD=&AMT=&CURRENCYCODE=&AVSCODE=&CVV2MATCH=&TRANSACTIONID=';
-  
+
   public function paymentDataRequirements() {
     return array('Amount', 'Currency');
   }
-  
+
   /**
    * Generate a dummy NVP response string with some pre-set value
-   * 
+   *
    * @param $data - The payment data; $ack - The desired ACK code, default to Success
-   * 
-   *  @return the dummy response NVP string 
+   *
+   *  @return the dummy response NVP string
    */
   public function generateDummyResponse($data, $ack = null) {
     $templateArr = $this->parseResponse($this->responseTemplate);
@@ -72,14 +82,14 @@ class PayPalDirectGateway_Mock extends PayPalDirectGateway {
     $templateArr['AVSCODE'] = 'X';
     $templateArr['CVV2MATCH'] = 'M';
     $templateArr['TRANSACTIONID'] = '1000';
-    
+
     return http_build_query($templateArr);
   }
-  
+
   public function process($data) {
     $amount = $data['Amount'];
     $cents = round($amount - intval($amount), 2);
-    
+
     switch ($cents) {
       case 0.00:
         return new RestfulService_Response($this->generateDummyResponse($data, 'Success'));
